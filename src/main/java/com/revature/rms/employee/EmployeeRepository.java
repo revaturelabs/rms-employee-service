1751b90523d070e2b8c1a04af8f6c21b36ca8a6a
Wezley <wezley.singleton@gmail.com>
2020-03-14 22:34:36 -0400

    added EmployeeRepo

--- src/main/java/com/revature/rms/employee/EmployeeRepository.java
+++ src/main/java/com/revature/rms/employee/EmployeeRepository.java
@@ -1,4 +1,17 @@
 package com.revature.rms.employee;
 
-public class EmployeeRepository {
+import com.revature.rms.employee.entities.Department;
+import com.revature.rms.employee.entities.Employee;
+import com.revature.rms.employee.entities.Title;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface EmployeeRepository extends ReactiveMongoRepository<Employee, String> {
+
+    Flux<Employee> findEmployeesByDepartment(Department dept);
+    Flux<Employee> findEmployeesByTitle(Title title);
+
 }
