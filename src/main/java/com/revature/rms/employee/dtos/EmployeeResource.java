5a6a51403afcf726066defba8e59c75d01011042
Wezley <wezley.singleton@gmail.com>
2020-03-14 22:32:44 -0400

    adding dtos

--- src/main/java/com/revature/rms/employee/dtos/EmployeeResource.java
+++ src/main/java/com/revature/rms/employee/dtos/EmployeeResource.java
@@ -1,37 +1,122 @@
 package com.revature.rms.employee.dtos;
 
-import com.revature.rms.employee.entities.Department;
+import com.revature.rms.employee.entities.Employee;
 import com.revature.rms.employee.entities.ResourceMetadata;
-import com.revature.rms.employee.entities.Title;
 
-public class EmployeeDTO {
-    
-    private Integer id;
+import java.util.Objects;
+
+// TODO add JSR-303 annotations
+
+public class EmployeeResource {
+
+    private String id;
     private String firstName;
     private String lastName;
     private String email;
-    private Title title;
-    private Department department;
+    private String title;
+    private String department;
     private ResourceMetadata metadata;
 
-    public EmployeeDTO(String fn, String ln, String email, Title title, Department dept, ResourceMetadata metadata) {
-        this.firstName = fn;
-        this.lastName = ln;
-        this.email = email;
-        this.title = title;
-        this.department = dept;
-        this.metadata = metadata;
+    public EmployeeResource() {
+        super();
     }
 
-    public EmployeeDTO(Integer id, String fn, String ln, String email, Title title, Department dept, ResourceMetadata metadata) {
+    public EmployeeResource(Employee employee) {
+        this.id = employee.getId();
+        this.firstName = employee.getFirstName();
+        this.lastName = employee.getLastName();
+        this.email = employee.getEmail();
+        this.title = employee.getTitle().toString();
+        this.department = employee.getDepartment().toString();
+        this.metadata = employee.getMetadata();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
         this.id = id;
-        this.firstName = fn;
-        this.lastName = ln;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
         this.title = title;
-        this.department = dept;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public ResourceMetadata getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(ResourceMetadata metadata) {
         this.metadata = metadata;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeeResource that = (EmployeeResource) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(firstName, that.firstName) &&
+                Objects.equals(lastName, that.lastName) &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(title, that.title) &&
+                Objects.equals(department, that.department) &&
+                Objects.equals(metadata, that.metadata);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, title, department, metadata);
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeResource{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", title='" + title + '\'' +
+                ", department='" + department + '\'' +
+                ", metadata=" + metadata +
+                '}';
+    }
 
 }
--- src/main/java/com/revature/rms/employee/dtos/GetEmployeeByFieldRequest.java
+++ src/main/java/com/revature/rms/employee/dtos/GetEmployeeByFieldRequest.java
@@ -1,4 +1,62 @@
 package com.revature.rms.employee.dtos;
 
+import javax.validation.constraints.NotEmpty;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class GetEmployeeByFieldRequest {
+
+    @NotEmpty
+    private String field;
+
+    @NotEmpty
+    private String[] values;
+
+    public GetEmployeeByFieldRequest() {
+        super();
+    }
+
+    public GetEmployeeByFieldRequest(String field, String[] values) {
+        this.field = field;
+        this.values = values;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String[] getValues() {
+        return values;
+    }
+
+    public void setValues(String[] value) {
+        this.values = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GetEmployeeByFieldRequest that = (GetEmployeeByFieldRequest) o;
+        return Objects.equals(field, that.field) &&
+                Objects.equals(values, that.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(field, values);
+    }
+
+    @Override
+    public String toString() {
+        return "GetEmployeeByFieldRequest{" +
+                "field='" + field + '\'' +
+                ", values=" + Arrays.toString(values) +
+                '}';
+    }
+
 }
