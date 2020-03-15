8ae05ac7652e7e5b0a3f07d43e97a65618bfc976
Wezley <wezley.singleton@gmail.com>
2020-03-14 22:34:19 -0400

    add EmployeeController

--- src/main/java/com/revature/rms/employee/EmployeeController.java
+++ src/main/java/com/revature/rms/employee/EmployeeController.java
@@ -1,4 +1,51 @@
 package com.revature.rms.employee;
 
+import com.revature.rms.employee.dtos.EmployeeResource;
+import com.revature.rms.employee.dtos.GetEmployeeByFieldRequest;
+import com.revature.rms.employee.dtos.NewEmployeeRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+// TODO document class and methods
+
+@RestController
+@RequestMapping("/employees")
 public class EmployeeController {
+
+    private EmployeeServiceImpl employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeServiceImpl service) {
+        this.employeeService = service;
+    }
+
+    @GetMapping
+    public Flux<EmployeeResource> getAllEmployees() {
+        return this.employeeService.getAllEmployees();
+    }
+
+    @GetMapping(value = "/field")
+    public Flux<EmployeeResource> getEmployeesByField(@RequestBody @Valid GetEmployeeByFieldRequest request) {
+        return this.employeeService.getEmployeesByField(request.getField(), request.getValues());
+    }
+
+    @PostMapping
+    public Mono<EmployeeResource> saveNewEmployee(@RequestBody @Valid NewEmployeeRequest newEmployee) {
+        return this.employeeService.saveNewEmployee(newEmployee);
+    }
+
+    @PatchMapping
+    public Mono<Void> updateEmployee(@RequestBody @Valid EmployeeResource updatedEmployee) {
+        return this.employeeService.updateEmployee(updatedEmployee);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public Mono<Void> deactivateEmployeeById(@PathVariable String id) {
+        return this.employeeService.deactivateEmployeeById(id);
+    }
+
 }
