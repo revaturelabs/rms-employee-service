86ac3de831a0795b951bd28467fcb54a94ccd5a0
Wezley <wezley.singleton@gmail.com>
2020-03-14 22:31:54 -0400

    rename to AppConfig

--- src/main/java/com/revature/rms/employee/AppConfig.java
+++ src/main/java/com/revature/rms/employee/AppConfig.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class EmployeeServiceApplication {
+public class AppConfig {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeServiceApplication.class, args);
+		SpringApplication.run(AppConfig.class, args);
 	}
 
 }
