7c6195ae759589a83d73b5dde39eaf8b60a0c78a
Wezley <wezley.singleton@gmail.com>
2020-03-15 22:32:38 -0400

    added resourcenotfound exception

---
+++ src/main/java/com/revature/rms/employee/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.revature.rms.employee.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String s) {
+        super(s);
+    }
+
+}
