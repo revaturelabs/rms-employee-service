13b05771c64c23eb72c1768249315b1c79c4f37e
Wezley <wezley.singleton@gmail.com>
2020-03-20 21:40:45 -0400

    moved impl to ResourceController from rms-core

--- src/main/java/com/revature/rms/employee/controllers/EmployeeController.java
+++ src/main/java/com/revature/rms/employee/controllers/EmployeeController.java
@@ -1,16 +1,14 @@
 package com.revature.rms.employee.controllers;
 
-import com.revature.rms.core.controllers.AutoController;
+import com.revature.rms.core.controllers.ResourceController;
 import com.revature.rms.employee.entities.Employee;
 import com.revature.rms.employee.services.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employees")
-public class EmployeeController extends AutoController<Employee> {
+public class EmployeeController extends ResourceController<Employee> {
 
-    @Autowired
     public EmployeeController(EmployeeService service) {
         super(service);
     }
