06013a7e9ee6651348c577506a9b7a481a0777d4
Wezley <wezley.singleton@gmail.com>
2020-03-20 21:41:33 -0400

    moved impl to ResourceService from rms-core

--- src/main/java/com/revature/rms/employee/services/EmployeeService.java
+++ src/main/java/com/revature/rms/employee/services/EmployeeService.java
@@ -1,20 +1,14 @@
 package com.revature.rms.employee.services;
 
-import com.revature.rms.core.services.AutoService;
+import com.revature.rms.core.services.ResourceService;
 import com.revature.rms.employee.repos.EmployeeRepository;
 import com.revature.rms.employee.entities.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.stereotype.Service;
 
-// TODO document class and methods
-
 @Service
-public class EmployeeService extends AutoService<Employee> {
+public class EmployeeService extends ResourceService<Employee> {
 
-    private ReactiveMongoTemplate mongoTemplate;
-
-    @Autowired
     public EmployeeService(EmployeeRepository repo, ReactiveMongoTemplate template) {
         super(repo, template);
     }
