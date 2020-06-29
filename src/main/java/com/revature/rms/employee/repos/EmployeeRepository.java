2ae80e2ac8f48d54de9cd2440b8de0ad68989701
wezley <wezley.singleton@gmail.com>
2020-06-29 01:30:28 -0400

    added basic domain queries to repo

--- src/main/java/com/revature/rms/employee/repos/EmployeeRepository.java
+++ src/main/java/com/revature/rms/employee/repos/EmployeeRepository.java
@@ -1,10 +1,17 @@
 package com.revature.rms.employee.repos;
 
+import com.revature.rms.employee.documents.Department;
 import com.revature.rms.employee.documents.Employee;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface EmployeeRepository extends ReactiveMongoRepository<Employee, String> {
+
+    Flux<Employee> findEmployeesByDepartment(Department department);
+    Flux<Employee> findEmployeesByManagerId(String managerId);
+    Mono<Employee> findEmployeeByEmail(String email);
 
 }
