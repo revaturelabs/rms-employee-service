50108187300702867840786841e173e8868420e5
wezley <wezley.singleton@gmail.com>
2020-06-29 01:29:43 -0400

    added logging aspect (need to look into improving implementation)

--- src/main/java/com/revature/rms/employee/util/EmployeeServiceLoggingAspect.java
+++ src/main/java/com/revature/rms/employee/util/EmployeeServiceLoggingAspect.java
@@ -1,4 +1,10 @@
 package com.revature.rms.employee.util;
 
-public class EmployeeServiceLoggingAspect {
+import com.revature.rms.core.util.LoggingAspect;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class EmployeeServiceLoggingAspect extends LoggingAspect {
 }
