a8aa63595694b69683bd183fe8fb7c96d7ea074a
wezley <wezley.singleton@gmail.com>
2020-06-29 01:29:00 -0400

    added endpoints to get gmployees by department and manager id

--- src/main/java/com/revature/rms/employee/controllers/EmployeeController.java
+++ src/main/java/com/revature/rms/employee/controllers/EmployeeController.java
@@ -1,16 +1,32 @@
 package com.revature.rms.employee.controllers;
 
 import com.revature.rms.core.controllers.ResourceController;
+import com.revature.rms.core.exceptions.NoImplementationException;
+import com.revature.rms.core.models.Resource;
 import com.revature.rms.employee.documents.Employee;
 import com.revature.rms.employee.services.EmployeeService;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController extends ResourceController<Employee> {
 
+    private EmployeeService employeeService;
+
     public EmployeeController(EmployeeService service) {
         super(service);
+        this.employeeService = service;
+    }
+
+    @GetMapping(value="/department/{dept}")
+    public Flux<Employee> getEmployeesByDepartment(@PathVariable String dept) {
+        return employeeService.findEmployeesByDepartment(dept);
+    }
+
+    @GetMapping(value="/managerId/{managerId}")
+    public Flux<Employee> getEmployeesByManagerId(@PathVariable String managerId) {
+        return employeeService.findEmployeesByManagerId(managerId);
     }
 
 }
